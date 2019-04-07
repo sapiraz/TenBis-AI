aebfbc90ac04ce5896c5f397e882c48ec586fcea
omrishalev22 <omrimexico@gmail.com>
2019-04-07 20:41:16 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+function sendSMS (name){
+    var TMClient = require('textmagic-rest-client');
+
+    console.log(name);
+
+/*    var c = new TMClient('maximparamonov', 'dIhK5GNIosHN9uJfF5pGBDOu0kNTKq');
+
+    c.Messages.send({text: 'test message from code', phones:'972523982995'}, function(err, res){
+
+        console.log('Messages.send()', err, res);*/
+
+}
